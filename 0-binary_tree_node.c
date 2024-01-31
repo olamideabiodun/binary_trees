a7fd5383dcb372a79460b023178e69963c81c326
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2024-01-31 17:13:44 +0200

    Betty Checked

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -18,6 +18,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	newNode->parent = parent;
 	newNode->left = NULL;
 	newNode->right = NULL;
-	return (newNod);
+	return (newNode);
 }
 
