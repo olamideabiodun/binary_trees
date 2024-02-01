74ac009342bb6cfb686387c92e8519c05c11ab8f
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2024-02-01 22:49:41 +0200

    Vscode

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,10 +1,14 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - returns the total size of a binary tree
+ * binary_tree_size - returns total size of a binary tree
  * @tree: pointer to the root node of the tree to measure
+ * Return: the size of the tree else 0 on failure
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	return ((tree && tree->parent) ? 1 + binary_tree_size(tree->right) + binary_tree_size(tree->left) + 1 : 0);
+	if (tree)
+	{
+		return (1 + binary_tree_size(tree->right) + binary_tree_size(tree->left));
+	}
+	return (0);
 }
-
